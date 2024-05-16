2360d5221844b4181a8806a693f45371e8a751d1
Mabroor <mabroorahmed18@gmail.com>
2024-05-16 17:51:04 +0100

    landing page css inprogress

--- app/page.tsx
+++ app/page.tsx
@@ -8,7 +8,7 @@ export default function Home() {
   return (
     <>
     <main className=" max-w-7xl max-h-lvh mx-auto p-3">
-      <nav className=" bg-slate-950 flex flex-row justify-between bg-origin-padding p-3 text-green-400">
+      <nav className=" flex flex-row justify-between bg-origin-padding p-3 text-white">
         <div className=" hover:text-white">Humanize-Text</div>
         <div className="flex">
           <div className="hover:text-white px-3">Pricing</div>
@@ -17,13 +17,19 @@ export default function Home() {
       </nav>
       <section className="flex flex-row justify-between py-32 ">
         <div className="flex flex-col justify-left min-h-16 w-3/5">
-          <div>
-            <h1 className="text-5xl text-green-600 font-sans font-extrabold  text-left mt-4 p-2 ">
-              Humanize your text to <span className=" bg-white text-green-500">remove ai detection</span>
+          <div className=" text-left mt-4 p-2">
+            <h1 className=" text-6xl leading-normal  text-white font-sans font-extrabold py-4 min">
+              Humanize your text &<br />remove {" "}
+               <span className=" bg-green-800 ">
+                ai detection
+              </span>
             </h1>
+            <p className="text-white">
+            The NextJS boilerplate with all you need to build your SaaS, AI tool, or any other web app and make your first $ online fast.
+            </p>
           </div>
-          <div className="py-4">
-            <Button size="lg" className=" bg-green-500 text-black-700">Get Humanize</Button>
+          <div className="p-2">
+            <Button size="lg" className=" bg-green-500 text-black-700 rounded">Get Humanize</Button>
           </div>
         </div>
 
@@ -33,7 +39,7 @@ export default function Home() {
 
           <div className="flex flex-row justify-between ">
             <div className="bg-gray-600 rounded w-5/6 p-2 text-white">Input text area</div>
-            <Button variant="outline" size="icon" >
+            <Button variant="destructive" size="icon" >
             <div>
               <CiSearch style={{color:"green", fontSize:25}}/>
             </div>
