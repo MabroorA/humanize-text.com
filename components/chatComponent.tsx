6f5bbf28ce0022afe237fcbaef12362d14b0e36a
Mabroor <mabroorahmed18@gmail.com>
2024-05-18 22:53:47 +0100

    chat component added

--- components/chatComponent.tsx
+++ components/chatComponent.tsx
@@ -2,9 +2,12 @@
 
 
 export default function chatComponent(){
+    
     return(
         <div>
-            <h1 className="text-white text-3xl text-center font-bold">Chat component </h1>
+            <h1 className="text-white text-3xl text-center font-bold">
+                Chat component 
+            </h1>
 
         </div>
     )
