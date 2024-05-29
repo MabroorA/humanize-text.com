c72d1cd6acc0afd595e18c74036194ad41c00f63
Mabroor <mabroorahmed18@gmail.com>
2024-05-29 20:21:58 +0100

    favicon added

--- app/page.tsx
+++ app/page.tsx
@@ -39,7 +39,7 @@ export default function Home() {
                 ]}
                 
                 repeat={Infinity}
-                speed={20}
+                speed={10}
                 deletionSpeed={20}
                 
                 />
