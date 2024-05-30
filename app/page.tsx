448f854a752856c53bf12133e7342ad3661668ae
Mabroor <mabroorahmed18@gmail.com>
2024-05-30 20:00:05 +0100

    layout changed on landing page

--- app/page.tsx
+++ app/page.tsx
@@ -17,68 +17,56 @@ export default function Home() {
   return (
     <>
 
-      <section className="flex flex-col justify-between py-8 ">
-        <div className="flex flex-col justify-center min-h-16  ">
-          <div className=" text-center ">
-            <h1 className=" text-6xl  text-white  font-extrabold py-3 ">
-                Bypass 
-               <span className="bg-clip-text text-transparent bg-gradient-to-r from-teal-300 to-violet-500 ">
-                {" "} 
-
-                <TypeAnimation
-                sequence={[
-                  
-                  'AI DETECTION', 
-                  3000, 
-                  'Turnit DETECTION', 
-                  3000, 
-                  'ChatGPT DETECTION',
-                  () => {
-                    console.log('Sequence completed');
-                  },
-                ]}
-                
-                repeat={Infinity}
-                speed={10}
-                deletionSpeed={20}
-                
-                />
-
-                
+      <section className="flex flex-row justify-around pt-20 ">
+        <div className="flex flex-col justify-center ">
+          <div className="text-left text-white ">
+            <h1 className="text-5xl font-extrabold ">
+            Save hours of writing <br/>Bypass 
+               <span className="text-transparent bg-clip-text bg-gradient-to-r from-teal-300 to-violet-500 ">
+                {" "} AI DETECTION
               </span>
+
             </h1>
+            <p className="py-3 pt-8 text-2xl">
+            Save hours of writing
+            </p>
+            
+          </div>
+          <div className="pt-8 ">
+            <Button size="xlg" variant="default" className="text-2xl text-black bg-clip bg-gradient-to-r from-teal-300 to-violet-500 ">
+                Get Humanize
+            </Button>
           </div>
         </div>
-
-        <div className="flex flex-col items-center   min-h-full min-w-full   ">
         
+
+        <div className="flex flex-col items-center ">
           <Image
-          className="w-5/6  rounded-2xl p-2"
-          
+          className="w-full p-2 rounded-2xl"
           src={demoGif}
           alt="image not loaded"
           />
         </div>
-        <div className="flex flex-row justify-center  p-2 ">
+        {/* <div className="flex flex-row justify-center p-2 ">
             <Image
-                className="bg-transparent mx-4"
+                className="mx-4 bg-transparent"
                 src={logoIcon}
                 width={30}
                 alt="logo"
                 priority
               />
-            <Button size="xlg" variant="default" className="  text-2xl text-black   bg-clip  bg-gradient-to-r from-teal-300 to-violet-500">
+            <Button size="xlg" variant="default" className="text-2xl text-black bg-clip bg-gradient-to-r from-teal-300 to-violet-500">
 
               Get Humanize
             </Button>
-          </div>
+          </div> */}
         
       </section>
       <WhyHumanize/>
       <div id="features">
         <Comparison/>
       </div>
-      <div className=" bg-black " id="pricing">
+      <div className="bg-black " id="pricing">
         <Pricing/>
       </div>
     </>
