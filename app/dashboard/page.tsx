44cea7b65f9f8f30d5b199aaf68d7068da75d33c
Mabroor <mabroorahmed18@gmail.com>
2024-06-06 23:37:21 +0100

    dashboard page is protected if not signed in

--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -1,16 +1,26 @@
 
 import ChatComponent from "@/components/chatComponent";
+import { auth } from "@/lib/auth";
+import { redirect } from "next/navigation";
 
 
 
-function page() {
+
+export default async function page() {
+
+  const session = await auth();
+  const user = session?.user;
+
+  if (!user) {
+    redirect("/api/auth/signin?callbackUrl=/dashboard")
+  }
+
 
   return (
     <>
-      <h1 className=' text-center text-3xl font-bold bg-clip-text text-transparent bg-gradient-to-r from-green-600 to-purple-400'>Dashboard</h1>
+      <h1 className='text-3xl font-bold text-center text-transparent  bg-clip-text bg-gradient-to-r from-green-600 to-purple-400'>Dashboard</h1>
       <ChatComponent/>
     </>
   )
 }
 
-export default page
