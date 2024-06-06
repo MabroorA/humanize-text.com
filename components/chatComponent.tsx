e572f50251544258840c6d002aeddc7fba4ad82f
Mabroor <mabroorahmed18@gmail.com>
2024-06-06 01:31:11 +0100

    mobile layout added for dashboard

--- components/chatComponent.tsx
+++ components/chatComponent.tsx
@@ -10,42 +10,43 @@ export default function ChatComponent(){
     const { messages, input, handleInputChange, handleSubmit } = useChat({
         api: 'api/chat',
       });
+
     return(
         <div>
-            <div  className=' flex flex-row justify-center p-10 w-full'>
+            <div  className='flex flex-col py-4 space-y-4 md:justify-center md:w-full md:p-10 md:flex-row'>
 
 
-                <form className='flex flex-col justify-end w-1/2 h-96 bg-gray-400 rounded-lg  m-0.5' onSubmit={handleSubmit}>
-                <div className='flex-grow overflow-y-auto p-3 m-0.5'>
-                {messages
-                    .filter(m => m.role === 'user') // Only display messages from the user
-                    .map(m => (
-                    <div key={m.id} className='mb-2 p-2 bg-white rounded'>
-                        {m.content}
+                <form className='flex flex-col mx-auto w-4/5  md:flex-col md:justify-end md:w-1/2 md:h-96 bg-gray-400 rounded-lg  md:m-0.5' onSubmit={handleSubmit}>
+                    <div className='flex-grow overflow-y-auto p-3 m-0.5'>
+                    {messages
+                        .filter(m => m.role === 'user') // Only display messages from the user
+                        .map(m => (
+                        <div key={m.id} className='p-2 mb-2 bg-white rounded'>
+                            {m.content}
+                        </div>
+                    ))}
                     </div>
-                ))}
-                </div>
 
-                <div className='flex flex-row justify-between p-3 m-0.5 '>
-                <textarea
-                    className='rounded  text-black w-full h-full px-2 m-0.5  bg-white  placeholder-black place-content-center  resize-none focus:outline-none focus:border-transparent'
-                    value={input}
-                    placeholder="Enter text to Humanize"
-                    onChange={handleInputChange}
-                />
-                <Button type='submit' size="lg" variant="outline"  className='bg-teal-500 h-full m-0.5 font-medium text-white bg-clip  bg-gradient-to-r from-teal-300 to-violet-500'>Humanize</Button>
-                </div>
+                    <div className='flex flex-col md:flex-row md:justify-between md:p-3 m-0.5 '>
+                    <textarea
+                        className='rounded  text-black w-full h-full px-2 m-0.5  bg-white  placeholder-black place-content-center  resize-none focus:outline-none focus:border-transparent'
+                        value={input}
+                        placeholder="Enter text to Humanize"
+                        onChange={handleInputChange}
+                    />
+                    <Button type='submit' size="lg" variant="outline"  className='bg-teal-500 md:h-full m-0.5 font-medium text-white bg-clip  bg-gradient-to-r from-teal-300 to-violet-500'>Humanize</Button>
+                    </div>
 
                 </form>
-
-                <div className='w-1/2 h-96 bg-gray-400 rounded-lg p-3 m-0.5 overflow-y-auto'>
-                {messages
-                    .filter(m => m.role === 'assistant') // Only display messages from the assistant
-                    .map(m => (
-                    <div key={m.id} className='mb-2 p-2 bg-white rounded'>
-                        {m.content}
-                    </div>
-                ))}
+                
+                <div className='flex flex-col w-4/5 mx-auto  md:h-96 bg-gray-400 rounded-lg md:p-3 md:m-0.5 md:overflow-y-auto'>
+                    {messages
+                        .filter(m => m.role === 'assistant') // Only display messages from the assistant
+                        .map(m => (
+                        <div key={m.id} className='p-2 mb-2 font-medium text-white rounded'>
+                            {m.content}
+                        </div>
+                    ))}
                 </div>
             </div>
 
