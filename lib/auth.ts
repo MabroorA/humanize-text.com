72c1dd2d14d2ba9224c10639e3aa59aa76f23795
Mabroor <mabroorahmed18@gmail.com>
2024-06-08 15:27:12 +0100

    supabase connected to prisma

--- lib/auth.ts
+++ lib/auth.ts
@@ -1,18 +1,19 @@
-import { SupabaseAdapter } from "@auth/supabase-adapter";
-import { Adapter } from "next-auth/adapters";
+// import { SupabaseAdapter } from "@auth/supabase-adapter";
+// import { Adapter } from "next-auth/adapters";
 import NextAuth from "next-auth";
 import authConfig from "./auth.config";
 
-export const {
-    handlers: { GET, POST },
-    auth,
-    signIn,
-    signOut 
-} = NextAuth({
-    adapter: SupabaseAdapter({
-      url: process.env.NEXT_PUBLIC_SUPABASE_URL ?? "",
-      secret: process.env.SUPABASE_SERVICE_ROLE_KEY ?? "",
-    })as Adapter,
-    // CALLBACKS
-    ...authConfig
+import { PrismaAdapter } from "@auth/prisma-adapter"
+import prisma from "./db";
+ 
+
+
+export const { handlers: { GET, POST } ,auth,signIn,signOut } = NextAuth({
+  adapter: PrismaAdapter(prisma),
+  // adapter: SupabaseAdapter({
+  //   url: process.env.NEXT_PUBLIC_SUPABASE_URL ?? "",
+  //   secret: process.env.SUPABASE_SERVICE_ROLE_KEY ?? "",
+  // })as Adapter,
+  // CALLBACKS
+  ...authConfig
   })
